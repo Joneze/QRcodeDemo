2ac9277986bfad1449f70b554fcd764e1c40c852
jay <zenghui64@qq.com>
2017-10-30 16:47:41 +0800

    初步调用摄像头扫描

--- QRCodeDemo/QRCodeDemo/QRCodeViewController.m
+++ QRCodeDemo/QRCodeDemo/QRCodeViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "QRCodeViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface QRCodeViewController ()
+@interface QRCodeViewController ()<AVCaptureMetadataOutputObjectsDelegate>
+
+@property (strong,nonatomic)AVCaptureDevice * device;
+@property (strong,nonatomic)AVCaptureDeviceInput * input;
+@property (strong,nonatomic)AVCaptureMetadataOutput * output;
+@property (strong,nonatomic)AVCaptureSession * session;
+@property (strong,nonatomic)AVCaptureVideoPreviewLayer * preview;
 
 @end
 
@@ -17,6 +24,76 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    [self performSelector:@selector(setupCamera) withObject:nil afterDelay:0.3];
+    
+}
+
+-(void)setupCamera
+{
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    if (device==nil) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"提示" message:@"设备没有摄像头" preferredStyle:UIAlertControllerStyleAlert];
+        [alert addAction:[UIAlertAction actionWithTitle:@"确认" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            
+        }]];
+        [self presentViewController:alert animated:YES completion:nil];
+        return;
+    }
+    // Device
+    _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    
+    // Input
+    _input = [AVCaptureDeviceInput deviceInputWithDevice:self.device error:nil];
+    
+    // Output
+    _output = [[AVCaptureMetadataOutput alloc]init];
+    [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    
+    //设置扫描区域
+//    CGFloat top = TOP/SCREEN_HEIGHT;
+//    CGFloat left = LEFT/SCREEN_WIDTH;
+//    CGFloat width = 220/SCREEN_WIDTH;
+//    CGFloat height = 220/SCREEN_HEIGHT;
+    ///top 与 left 互换  width 与 height 互换
+    [_output setRectOfInterest:CGRectMake(0.5,0.5, 0.5, 0.5)];
+    
+    
+    // Session
+    _session = [[AVCaptureSession alloc]init];
+    [_session setSessionPreset:AVCaptureSessionPresetHigh];
+    
+    if ([_session canAddInput:self.input])
+    {
+        [_session addInput:self.input];
+    }
+    
+    if ([_session canAddOutput:self.output])
+    {
+        [_session addOutput:self.output];
+    }
+    
+    // 条码类型 AVMetadataObjectTypeQRCode
+    [_output setMetadataObjectTypes:[NSArray arrayWithObjects:AVMetadataObjectTypeQRCode, nil]];
+    
+    // Preview
+    _preview =[AVCaptureVideoPreviewLayer layerWithSession:_session];
+    _preview.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    _preview.frame =self.view.layer.bounds;
+    [self.view.layer insertSublayer:_preview atIndex:0];
+    
+    // Start
+    [_session startRunning];
+}
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
+{
+    NSString *stringValue;
+    if ([metadataObjects count] >0){
+        //停止扫描
+        [_session stopRunning];
+        AVMetadataMachineReadableCodeObject * metadataObject = [metadataObjects objectAtIndex:0];
+        stringValue = metadataObject.stringValue;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
